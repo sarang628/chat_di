aff6f412f214468f9860509f0d28e33083937dfe
sryang <sarang628@gmail.com>
2024-10-03 09:36:32 +0800

    update

--- ChatActivity.kt
+++ ChatActivity.kt
@@ -1,17 +1,22 @@
 package com.sarang.torang.di.chat_di
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import com.google.samples.apps.sunflower.ui.TorangTheme
+import com.sarang.instagralleryModule.GalleryNavHost
+import com.sarang.torang.compose.bottomsheet.ImageSelectBottomSheetScaffold
 import com.sarang.torang.compose.chat.ChatScreen
 import com.sarang.torang.di.image.provideTorangAsyncImage
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ChatActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,10 +27,21 @@ class ChatActivity : ComponentActivity() {
         setContent {
             TorangTheme {
                 ChatScreen(
-                    onBack = {
-                        finish()
-                    }, image = provideTorangAsyncImage(),
-                    roomId = roomId
+                    onBack = { finish() },
+                    image = provideTorangAsyncImage(),
+                    roomId = roomId,
+                    galleryCompose = {
+                        GalleryNavHost(onNext = {}, onClose = { /*TODO*/ }, onBack = {})
+                    },
+                    galleryBottomSheetScaffoldCompose =
+                    { show, onHidden, sheetContent, content ->
+                        ImageSelectBottomSheetScaffold(
+                            show = show,
+                            onHidden = onHidden,
+                            imageSelectCompose = sheetContent,
+                            content = content
+                        )
+                    }
                 )
             }
         }
