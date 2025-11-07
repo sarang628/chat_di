c1d01555ebf2b3b085df4e37bc9cc99f2ab3d940
sryang <sarang628@gmail.com>
2025-11-07 10:00:42 +0900

    arrange code

--- GetChatRoomUseCaseImpl.kt
+++ GetChatRoomUseCaseImpl.kt
@@ -6,14 +6,12 @@ import com.sarang.torang.data.ChatRoom
 import com.sarang.torang.data.ChatUser
 import com.sarang.torang.data.User
 import com.sarang.torang.repository.ChatRepository
-import com.sarang.torang.repository.LoginRepository
 import com.sarang.torang.usecase.GetChatRoomUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import javax.inject.Singleton
 
@@ -24,29 +22,14 @@ class GetChatRoomUseCaseImpl {
     @Provides
     fun provideGetChatRoomUseCase(
         chatRepository: ChatRepository,
-        loginRepository: LoginRepository,
     ): GetChatRoomUseCase {
         return object : GetChatRoomUseCase {
-            val userNameFlow = loginRepository.loginUser
-            val uiStateFlow = chatRepository.getAllChatRoomsFlow().map {
-                chatRooms -> chatRooms.map { chatRoom ->
-                    chatRoom.uiState
-                }
-            }
-
-            val uiStateWithoutMe = uiStateFlow.combine(userNameFlow) { list, userName ->
-                list.map { uiState ->
-                    uiState.copy(
-                        list = uiState.list.filter {
-                            it.nickName != userName?.chatUser?.nickName
-                        }
-                    )
-                }
-            }
-
-
             override fun invoke(): Flow<List<ChatRoomUiState>> {
-                return uiStateWithoutMe
+                return chatRepository.getAllChatRoomsFlow().map { chatRooms ->
+                    chatRooms.map { chatRoom ->
+                        chatRoom.uiState
+                    }
+                }
             }
         }
     }
