ce16c270fa14dab41193eb7858525b215be124d1
sryang <sarang628@gmail.com>
2025-11-12 08:14:22 +0900

    add delete chatroom

---
+++ DeleteChatRoomUseCaseImpl.kt
@@ -0,0 +1,26 @@
+package com.sarang.torang.di.chat_di
+
+import com.sarang.torang.core.database.dao.chat.ChatRoomDao
+import com.sarang.torang.repository.ChatRepository
+import com.sarang.torang.usecase.DeleteChatRoomUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DeleteChatRoomUseCaseImpl {
+    @Singleton
+    @Provides
+    fun provideDeleteChatRoomUseCase(
+
+    ): DeleteChatRoomUseCase {
+        return object : DeleteChatRoomUseCase {
+            override suspend fun invoke(roomId: Int) {
+
+            }
+        }
+    }
+}
