d797afc3bcbcca0800afc03609cf72f1c59da967
sryang <sarang628@gmail.com>
2026-02-09 23:36:13 +0900

    add video review

--- CustomChatImageLoader.kt
+++ CustomChatImageLoader.kt
@@ -1,14 +1,18 @@
 package com.sarang.torang.di.chat_di
 
+import androidx.compose.ui.unit.dp
 import com.sarang.torang.compose.chat.ChatImageLoaderType
+import com.sarang.torang.di.image.TorangAsyncImageData
 import com.sarang.torang.di.image.provideTorangAsyncImage
 
 val CustomChatImageLoader : ChatImageLoaderType = {
     provideTorangAsyncImage().invoke(
-        it.modifier,
-        it.url,
-        it.iconSize,
-        it.progressSize,
-        it.contentScale
+        TorangAsyncImageData(
+            it.modifier,
+            it.url,
+            it.progressSize ?: 30.dp,
+            it.iconSize ?: 30.dp,
+            it.contentScale
+        )
     )
 }
